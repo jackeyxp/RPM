a9e728c581b0bb183ed3d8bac08d42bf07325db4
18059104@qq.com <huijia264>
2017-08-09 15:47:01 +0800

    新增了开启和停止通道命令

--- rpm_6.8/transmit-1.0.1/transmit.h
+++ rpm_6.8/transmit-1.0.1/transmit.h
@@ -49,6 +49,8 @@ enum {
   kCmd_PHP_Get_All_Client     = 12,
   kCmd_PHP_Get_Live_Server    = 13,
   kCmd_Live_Quit              = 14,
+  kCmd_PHP_Start_Camera       = 15,
+  kCmd_PHP_Stop_Camera        = 16,
 };
 //
 // define the command header...
