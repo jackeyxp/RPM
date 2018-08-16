2c0119a224e9666896f5445b74b4b473bde1a445
18059104@qq.com <huijia264>
2018-08-16 09:31:43 +0800

    对日志文件进行切割分片

--- rpm_6.8/udpserver-1.0.1/udpserver.c
+++ rpm_6.8/udpserver-1.0.1/udpserver.c
@@ -2,17 +2,29 @@
 #include "app.h"
 #include "bmem.h"
 
+#include<sys/stat.h>
+ 
 // STL must use g++...
 // g++ -g udpserver.c bmem.c thread.cpp app.cpp tcpcamera.cpp tcproom.cpp tcpclient.cpp tcpthread.cpp room.cpp network.cpp student.cpp teacher.cpp -o udpserver -lrt -lpthread -ljson
 // valgrind --tool=memcheck --leak-check=full --show-reachable=yes ./udpserver
 
 CApp theApp;
 
+#define LOG_MAX_SIZE                2048            // 单条日志最大长度(字节)...
+#define LOG_MAX_FILE    40 * 1024 * 1024            // 日志文件最大长度(字节)...
+
 char g_absolute_path[256] = {0};
-bool doGetLogPath(char * lpOutPath, int inSize);
+char g_log_file_path[256] = {0};
+void doSliceLogFile(struct tm * lpCurTm);
+bool doGetCurFullPath(char * lpOutPath, int inSize);
 
 int main(int argc, char **argv)
 {
+  // 获取进程当前运行完整路径...
+  if( !doGetCurFullPath(g_absolute_path, 256) )
+    return -1;
+  // 构造日志文件完整路径...
+  sprintf(g_log_file_path, "%s%s", g_absolute_path, "udpserver.log");
   // 增大文件打开数量...
   if( !theApp.doInitRLimit() )
     return -1;
@@ -31,8 +43,8 @@ int main(int argc, char **argv)
 // 返回全局的App对象...
 CApp * GetApp() { return &theApp; }
 
-// 获取日志全路径...
-bool doGetLogPath(char * lpOutPath, int inSize)
+// 获取当前进程全路径...
+bool doGetCurFullPath(char * lpOutPath, int inSize)
 {
   // 如果路径已经有效，直接返回...
   if( strlen(lpOutPath) > 0 )
@@ -53,18 +65,37 @@ bool doGetLogPath(char * lpOutPath, int inSize)
   if( lpOutPath[strlen(lpOutPath) - 1] != '/' ) {  
     lpOutPath[strlen(lpOutPath)] = '/';  
   }
-  // create udpserver.log file...
-  strcat(lpOutPath, "udpserver.log");
   return true;
+}
+
+// 对日志进行分片操作...
+void doSliceLogFile(struct tm * lpCurTm)
+{
+  // 如果日志路径、日志文件路径、输入时间戳无效，直接返回...
+  if( lpCurTm == NULL || strlen(g_absolute_path) <= 0 || strlen(g_log_file_path) <= 0 )
+    return;
+  // 获取日志文件的总长度，判断是否需要新建日志...
+  struct stat dtLog = {0};
+  if( stat(g_log_file_path, &dtLog) < 0 )
+    return;
+  // 日志文件不超过40M字节，直接返回...
+  if( dtLog.st_size <= LOG_MAX_FILE )
+    return;
+  // 构造分片文件全路径名称...
+  char szNewSliceName[256] = {0};
+  sprintf(szNewSliceName, "%s%s_%d-%02d-%02d_%02d-%02d-%02d.log", g_absolute_path, "udpserver",
+          1900 + lpCurTm->tm_year, 1 + lpCurTm->tm_mon, lpCurTm->tm_mday,
+          lpCurTm->tm_hour, lpCurTm->tm_min, lpCurTm->tm_sec);
+  // 对日志文件进行重新命名操作...
+  rename(g_log_file_path, szNewSliceName);
 }
 
 // 全新的日志处理函数...
 bool do_trace(const char * inFile, int inLine, bool bIsDebug, const char *msg, ...)
 {
-  // 获取日志全路径...
-  doGetLogPath(g_absolute_path, 256);
-  if( strlen(g_absolute_path) <= 0 ) {
-    fprintf(stderr, "doGetLogPath failed\n");
+  // 获取日志文件全路径是否有效...
+  if( strlen(g_log_file_path) <= 0 ) {
+    fprintf(stderr, "log_file_path failed\n");
     return false;
   }
   // 准备日志头需要的时间信息...
@@ -76,6 +107,8 @@ bool do_trace(const char * inFile, int inLine, bool bIsDebug, const char *msg, .
   if((tm = localtime(&tv.tv_sec)) == NULL) {
     return false;
   }
+  // 对日志进行分片操作...
+  doSliceLogFile(tm);
   // 准备完整的日志头信息...
   int  log_size = -1;
   char log_data[LOG_MAX_SIZE] = {0};
@@ -90,7 +123,7 @@ bool do_trace(const char * inFile, int inLine, bool bIsDebug, const char *msg, .
   // 打开日志文件...
   int file_log_fd = -1;
   if( !bIsDebug ) {
-    file_log_fd = open(g_absolute_path, O_RDWR|O_CREAT|O_APPEND, 0666);
+    file_log_fd = open(g_log_file_path, O_RDWR|O_CREAT|O_APPEND, 0666);
     if( file_log_fd < 0 ) {
       return false;
     }
